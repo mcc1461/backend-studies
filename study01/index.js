a30080353510ca73ed7127b95404272142ab856b
mcc1461 <mcoskuncelebi@gmail.com>
2024-02-21 17:12:01 +0300

    JAVASCRIPT five questions.

--- study01/index.js
+++ study01/index.js
@@ -24,27 +24,30 @@ console.log(Number(0.1 + 0.2) == 0.3);
 // 3. Write program to find the sum of positive numbers. But if the user enters a negative numbers, the
 // loop ends, if the negative number entered is not added to sum
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-let sum = 0;
+// let sum = 0;
 
-const getNumber = () => {
-  readline.question('Enter a positive number (Enter a negative number to stop): ', number => {
-      const num = parseInt(number);
-      if (num < 0) {
-          console.log(`Sum of positive numbers: ${sum}`);
-          readline.close();
-      } else {
-          sum += num;
-          getNumber(); // Tekrar sayı iste
-      }
-  });
-};
+// const getNumber = () => {
+//   readline.question('Enter a positive number (Enter a negative number to stop): ', (number) => {
+//       const num = parseInt(number);
+//       if (num < 0) {
+//           console.log(`Sum of positive numbers: ${sum}`);
+//           readline.close();
+//       } else if (num == undefined || num == null || isNaN(num)) {
+//           getNumber(); // Tekrar sayı iste
+//       }
+//       else {
+//           sum += num;
+//           getNumber(); // Tekrar sayı iste
+//       }
+//   });
+// };
 
-getNumber(); // İlk sayıyı iste
+// getNumber(); // İlk sayıyı iste
 
 
 // Define a variable to hold the sum
@@ -110,7 +113,7 @@ getNumber(); // İlk sayıyı iste
 //   n++;
 // }
 // console.log(hash);
-console.log(2+null)
+// console.log(2+null)
 // 6. What is the output of the following code block ?
 // let firstName = null
 // let lastName = null
@@ -205,4 +208,104 @@ console.log(2+null)
 // let felix=new Cat("felix",5)
 // console.log(felix.sound())
 
+
+// EMRE PART
+
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the
+// number you passed to the function. For example, if the input is 4 then your function should
+// return 10 because 1 + 2 + 3 + 4 = 10.
+// Examples
+// addUp(4) - 10
+// addUp(13) - 91
+// addUp(600) - 180300
+
+// This challenge will help you interpret mathematical relationships both algebraically and
+// geometrically.
+// Create a function that takes a number (step) as an argument and returns the number of
+// matchsticks in that step. See step 1, 2 and 3 in the image above.
+// Examples
+// matchHouses(1) - 6
+// matchHouses(4) - 21
+// matchHouses(87) - 436
+
+// Create a function that takes an array of numbers and return both the minimum and
+// maximum numbers, in that order.
+// Examples
+// minMax([1, 2, 3, 4, 5]) - [1, 5]
+// minMax([2334454, 5]) - [5, 2334454]
+// minMax([1]) - [1, 1]
+
+// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write
+// a function to detect what the word is, knowing the following rules:
+// . The wanted word is in lowercase.
+// . The crowd of letters is all in uppercase.
+// . Note that the word will be spread out amongst the random letters, but their letters
+// remain in the same order.
+// Examples
+// detectWord("UcUNFYGaFYFYGtNUH") - "cat"
+// detectWord("bEEFGBuFBRrHgUHINFYaYr") - "burglar"
+// detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment") - "embezzlement"
+
+// Create a function that takes two dates and returns the number of days between the first
+// and second date.
+// Examples
+// getDays(
+// new Date("June 14, 2019"),
+// new Date("June 20, 2019")
+// - 6
+// getDays(
+// new Date("December 29, 2018"),
+// new Date("January 1, 2019")
+// - 3
+// // Dates may not all be in the same month/year.
+// getDays(
+// new Date("July 20, 2019"),
+// new Date("July 30, 2019")
+// - 10
+
+
+// 1. Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// function addUp(num) {
+//     let sum = 0
+//     for(let i =1; i<=num;i++){
+//         sum+=i
+//     }
+//     return sum
+// }
+
+// console.log(addUp(3));
+
+// 2. Create a function that takes a number (step) as an argument and returns the number of matchsticks in that step. See step 1, 2 and 3 in the image above.
+
+// function kibrit(adım){
+//     if(adım===1){
+//         return 6;
+//     }else {
+//         return 5*adım+1
+//     }
+// }
+// console.log(kibrit(3));
+
+// 3.Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// function minMax(arr) {
+//     return [Math.min(...arr), Math.max(...arr)]
+// }
+
+// console.log(minMax([1,5,8,89,3,2,-1]));
+
+// 4.A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+
+// function detectWord(letter) {
+//     return letter.split("").filter(item=> item===item.toLowerCase()).join("")
+// }
+// console.log(detectWord("UcERTaTUIUIt"));
+
+// 5.Create a function that takes two dates and returns the number of days between the first and second date.
+
+// function getDays(date1, date2) {
+//     return Math.abs((date1-date2)/(1000*60*60*24))
+// }
+// console.log(getDays(new Date("june 14, 2019"), new Date("june 20, 2068")));
 
