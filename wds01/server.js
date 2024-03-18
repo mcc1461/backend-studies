fea14a3f6d228ef86e9c2b2a6aa94e100768585a
mcc1461 <mcoskuncelebi@gmail.com>
2024-03-18 21:16:46 +0300

    up

--- wds01/server.js
+++ wds01/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    console.log("GET /")
-    res.download('server.js')
+    console.log("Hi there...")
+    // res.send("Hello mcc")
+    // res.sendStatus(500)
+    // res.status(500).send("Error occurred")
+    // res.json({message:"Hello mcc"})
+    // res.download("server.js")
+    // res.render('index')
+    // res.render('index',{name:"Mcc",age:20})
+     res.render("index",{text : "HELLO MY FRIENDS!"})
     // res.send("Hello mcc")
 })
 app.listen (7799,()=>console.log("Server run : http://127.0.0.1:7799"))
