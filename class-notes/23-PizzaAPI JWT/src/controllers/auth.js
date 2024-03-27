ae1e39c64c73fab0fa20ce84df4dab883d156a9c
mcc1461 <mcoskuncelebi@gmail.com>
2024-03-27 22:04:56 +0300

    update

---
+++ class-notes/23-PizzaAPI JWT/src/controllers/auth.js
@@ -0,0 +1,134 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+// Auth Controller:
+
+const User = require('../models/user')
+const Token = require('../models/token')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+const jwt = require('jsonwebtoken')
+
+
+module.exports = {
+
+    login: async (req, res) => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "aA?123456",
+                }
+            }
+        */
+
+        const { username, email, password } = req.body
+
+        if ((username || email) && password) {
+
+            const user = await User.findOne({ $or: [{ username }, { email }] })
+
+            if (user && user.password == passwordEncrypt(password)) {
+
+                if (user.isActive) {
+
+                    /* SIMPLE TOKEN */
+
+                    let tokenData = await Token.findOne({ userId: user.id })
+
+                    if (!tokenData) tokenData = await Token.create({
+                        userId: user.id,
+                        token: passwordEncrypt(user.id + Date.now())
+                    })
+
+                    /* SIMPLE TOKEN */
+
+                    /* JWT */
+
+                    accessInfo = {
+                        key : process.env.ACCESS_KEY,
+                        time : "30dm",
+                        data : {
+                            id : user.id,
+                            username : user.username,
+                            email : user.email,
+                            password : user.password,
+                            isAdmin : user.isAdmin,
+                            isActive : user.isActive
+                        }
+
+                    }
+                    refreshInfo = {
+                        key : process.env.REFRESH_KEY,
+                        time : "3d",
+                        data : {
+                            id : user.id,
+                            password : user.password
+                        }
+                    }
+
+                    // jwt.sign(payload, secretOrPrivateKey, [options, callback])
+                    // jwt.verify(token, secretOrPublicKey, [options, callback]) 
+
+                    const accessToken = jwt.sign(accessInfo.data, accessInfo.key, { expiresIn: accessInfo.time })
+                    const refreshToken = jwt.sign(refreshInfo.data, refreshInfo.key, { expiresIn: refreshInfo.time })
+
+                    
+
+
+                    /* JWT */
+
+
+                    res.status(200).send({
+                        error: false,
+                        token: tokenData.token,
+                        bearer: {
+                            access : accessToken,
+                            refresh : refreshToken
+                        },
+                        user
+                    })
+
+                } else {
+
+                    res.errorStatusCode = 401
+                    throw new Error('This account is not active.')
+
+                }
+            } else {
+
+                res.errorStatusCode = 401
+                throw new Error('Wrong username/email or password.')
+            }
+        } else {
+
+            res.errorStatusCode = 401
+            throw new Error('Please enter username/email and password.')
+        }
+
+    },
+
+    logout: async (req, res) => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "simpleToken: Logout"
+            #swagger.description = 'Delete token key.'
+        */
+
+        const auth = req.headers?.authorization // Token ...tokenKey...
+        const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...']
+        const result = await Token.deleteOne({ token: tokenKey[1] })
+
+        res.send({
+            error: false,
+            message: 'Token deleted. Logout was OK.',
+            result
+        })
+    }
+
+}
